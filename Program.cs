2bdf9cdd17e87ccd9e2d931750cbf9167503a363
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-05-17 13:04:44 -0300

    Código Base

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ComprasNoSupermercado
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Quantas Listas de Compras você deseja criar agora?");
+    
+    //Quantidade de listas a serem criadas
+    int numeroDeTestes = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Escreva a Lista de Compra separando cada Item com um ESPAÇO");
+    Console.WriteLine("Exemplo: carne suco.de.laranja pao.integral acucar");
+    Console.WriteLine("Aperte ENTER no final de cada Lista de Compra");
+    Console.WriteLine(); 
+
+    for (int i = 0; i < numeroDeTestes; i++) {
+      List<string> listDeCompras = new List<String>(Console.ReadLine().Split(' ')); //o espaço será usado como separador dos itens da lista
+
+      //Elimina a duplicidade de itens da lista
+      List<string> listaSemDuplicados = listDeCompras.Distinct().ToList();
+
+      //Agrupa os itens em ordem alfabetica (por padrão)
+      listaSemDuplicados.Sort();
+
+      //Mostrar na tela a lista com seus itens ordenados e sem repetir algum item
+      foreach(string item in listaSemDuplicados)
+      {
+        Console.Write($"{item} ");
+      }
+
+      Console.WriteLine();
+    
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Listas_e_Arquivos
+Salvando a lista e CRUD com o Arquivo gerado
+
+Usarei a Lista de Compra do seguinte repositório como base para gerar e manipular o Arquivo: 
+https://github.com/willianmgbrdotnet/desafioDeCodigo_listaDeCompras
